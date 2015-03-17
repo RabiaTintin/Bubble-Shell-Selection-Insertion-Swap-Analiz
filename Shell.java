822986cde7c0bb38b3a22544aa6b6f5e10edecf9
RabiaTintin <rabiatintin@hotmail.com>
2015-03-17 16:47:19 +0200

    Shell.java

---
+++ Shell.java
@@ -0,0 +1,22 @@
+package sortingalgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author BMB
+ */
+public class SortingAlgorithm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sort srt =new Sort();
+        int[] input = srt.generateRandomNumbers(500);
+        //srt.bubbleSort(input);
+        //srt.insertionSort(input);
+        //srt.selectionSort(input);
+    }
+}
